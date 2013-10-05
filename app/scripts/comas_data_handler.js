a29b91ab3f6cf75c5be0e2202799a0025c5a0b2b
osoken <osoken@gmail.com>
2013-10-05 14:41:51 +0900

    made data handler js.

---
+++ app/scripts/comas_data_handler.js
@@ -0,0 +1,14 @@
+
+monster = function(data)
+{
+  var name = data.name;
+  var attack = data.attack;
+  var defence = data.defence;
+  var speed = data.speed;
+  var attribute = data.attribute;
+  var id = data.id;
+  
+};
+
+
+
